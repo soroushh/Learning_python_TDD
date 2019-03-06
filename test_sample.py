150cfc1f769cbd3a4f5a52afc0ad6885c081f97c
Soroush Khosravi <sorush.kh68@gmail.com>
2019-03-06 13:19:15 +0000

    Defining fizzbuzz as a method is working now.

--- test_sample.py
+++ test_sample.py
@@ -1,5 +1,15 @@
-def func(x):
-    return x + 1
+def fizzBuzz(x):
+    if(x%15==0):
+        return("fizzbuzz")
+    elif(x%3==0):
+        return("fizz")
+    elif(x%5==0):
+        return("buzz")
+    else:
+        return(x)
 
 def test_answer():
-    assert func(3) == 4
+    assert fizzBuzz(15)== "fizzbuzz"
+    assert fizzBuzz(3) == "fizz"
+    assert fizzBuzz(5) == "buzz"
+    assert fizzBuzz(11)== 11
